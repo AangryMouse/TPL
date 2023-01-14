c1ee955c00c4b563c570a1e5b566db0ae152a6da
AangryMouse <polinageass@mail.ru>
2023-01-14 20:49:30 +0500

    Add AsyncAwaitScanner.cs

---
+++ AsyncAwaitScanner.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.NetworkInformation;
+using System.Net.Sockets;
+using System.Threading.Tasks;
+
+namespace TPL
+{
+    public class AsyncAwaitScanner : IPScanner
+    {
+        public Task Scan(IPAddress[] ipAddrs, int[] ports)
+        {
+            return Task.WhenAll(ipAddrs.Select(ip => PingAddress(ip, ports)));
+        }  
+  
+        private static async Task PingAddress(IPAddress ipAddr, int[] ports, int timeout = 3000)  
+        {  
+            Console.WriteLine($"Pinging {ipAddr}");
+
+            var pingReply = await new Ping().SendPingAsync(ipAddr, timeout);
+            
+            Console.WriteLine($"Pinged {ipAddr}: {pingReply.Status}");
+            
+            var tasks = new List<Task>();
+            if (pingReply.Status == IPStatus.Success)
+            {
+                tasks.AddRange(ports.Select(port => CheckPort(ipAddr, port, timeout)));
+            }
+            await Task.WhenAll(tasks);
+        }
+  
+        private static async Task CheckPort(IPAddress ipAddr, int port, int timeout = 3000)  
+        {  
+            using var tcpClient = new TcpClient();
+            
+            Console.WriteLine($"Checking {ipAddr}:{port}");  
+            var portStatus = await tcpClient.ConnectAsync(ipAddr, port, timeout);  
+            Console.WriteLine($"Checked {ipAddr}:{port} - {portStatus}");  
+        }  
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
-﻿using System.Net;
+﻿using System;
+using System.Diagnostics;
+using System.Net;
 
 namespace TPL
 {
@@ -8,8 +10,21 @@ namespace TPL
 		{
 			var ipAddrs = new[] {IPAddress.Parse("192.168.0.1"), IPAddress.Parse("127.0.0.1")/*, Place your ip addresses here*/};
 			var ports = new[] {21, 25, 80, 443, 3389 };
+			
+			Scanner(new SequentialScanner(), ipAddrs, ports);
+			Scanner(new ParallelScanner(), ipAddrs, ports);
+			Scanner(new AsyncAwaitScanner(), ipAddrs, ports);
+			
+		}
 
-			new ParallelScanner().Scan(ipAddrs, ports).Wait();
+		private static void Scanner(IPScanner scanner, IPAddress[] ipAddrs, int[] ports)
+		{
+			var timer = new Stopwatch();
+			timer.Start();
+			scanner.Scan(ipAddrs, ports).Wait();
+			timer.Stop();
+			
+			Console.WriteLine($"{scanner.GetType()} time: {timer.ElapsedMilliseconds}ms");
 		}
 	}
 }
